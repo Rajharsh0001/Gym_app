4fd9c7b20fcb89fcfe660dd2afac5b94dd43f139
Rajharsh0001 <harsh821101@gmail.com>
2024-09-06 20:33:03 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <h3>crated by harsh raj</h3>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    
+    <section className='hero'>
+      <div className='content'>
+        <div className='title'>
+          <h1>Let's</h1>
+          <h1>get</h1>
+          <h1>moving</h1>
+        </div>
+        <div className="sub-title">
+          <p>Your journey to fittness starts here</p>
+          <p>Unleash the potental</p>
+        </div>
+        <div className="buttons">
+          <button>Start your journey</button>
+          <button>Discover your plan</button>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Hero
